86f3015bdc03bcfc64ede488168494f4116bb598
Jia Le <jle26531@gmail.com>
2023-10-26 14:22:37 +0000

    basic structure

--- app/app.py
+++ app/app.py
@@ -1,25 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__, template_folder='web')
+app = Flask(__name__, template_folder='client', static_folder='static')
 
+@app.route('/')
+def home():
+    return render_template('index.html', title='Home', message='lol')
 
-@app.route('/hello-world')
-def root():
-    return (
-        [
-            {
-                'id': 1,
-                'name': 'hello',
-                'hair': 'true',
-            }, 
-            {
-                'id':2,
-                'name': 'hi',
-                'hair': 'false'
-            }
-        ]
-    )
+@app.route('/login')
+def login():
+    return render_template('auth/login.html', title='Login')
 
+@app.route('/favicon.ico')
+def favicon():
+  return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
