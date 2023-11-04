394a4175e039df2860eab03362c1e49728a7e3b8
Jia Le <jle26531@gmail.com>
2023-11-04 07:50:20 +0000

    Member Update Function

--- app/member.py
+++ app/member.py
@@ -54,3 +54,39 @@ def delete_member(member_id):
         flash('Member not found!', category='error')
 
     return redirect(url_for('member.home'))
+
+@member.route('/update/<int:member_id>', endpoint='update_member', methods=['POST', 'GET'])
+@init_process
+@login_required
+def update_member(member_id):
+    member_to_update = Member.query.get(member_id)
+
+    if not member_to_update:
+        flash('Member not found!', category='error')
+        return redirect(url_for('member.home'))
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        gender = request.form.get('gender')
+        category = request.form.get('category')
+        ic = request.form.get('ic')
+
+        validate_ic = Member.query.filter_by(card_number=ic).first()
+
+        if validate_ic and validate_ic != member_to_update:
+            flash('IC cannot be duplicated, try again.', category='error')
+        else:
+            try:
+                member_to_update.name = name
+                member_to_update.gender = gender
+                member_to_update.category = category
+                member_to_update.card_number = ic
+
+                db.session.commit()
+                flash('Member updated successfully!', category='success')
+            except:
+                flash('Failed to update the member!', category='error')
+            return redirect(url_for('member.home'))
+
+    return render_template('member/update.html', nav=True, title='Update Member', member=member_to_update,
+        gender=[{'name':'-'}, {'name':'Female'}, {'name':'Male'}])
