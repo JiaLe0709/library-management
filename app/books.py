11cf8ba594d90fbac6488ebf08331b6748796b43
Jia Le <jle26531@gmail.com>
2023-11-04 15:37:38 +0000

    Added Dewey Data

--- app/books.py
+++ app/books.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import json
 
 books = Blueprint('books', __name__, template_folder='client', static_folder='static')
 
@@ -6,6 +7,23 @@ books = Blueprint('books', __name__, template_folder='client', static_folder='st
 def home():
     return render_template('books/home.html', title='Books')
 
-@books.route('/new', endpoint='new_books')
+@books.route('/new', endpoint='new_books', methods=['POST', 'GET'])
 def new_books():
-    return render_template('books/new.html', title='Books')
+    if request.method == 'POST':
+        title = request.form.get('title')
+        author = request.form.get('title')
+        isbn = request.form.get('title')
+        dewey = request.form.get('title')
+        category = request.form.get('title')
+        num_pages = request.form.get('title')
+        published_year = request.form.get('title')
+        publisher = request.form.get('title')
+        barcode = request.form.get('title')
+        currency = request.form.get('title')
+        price = request.form.get('title')
+        sources = request.form.get('title')
+        
+    with open('dewey.json') as json_file:
+        dewey_file = json.load(json_file)
+        
+    return render_template('books/new.html', title='Books', dewey=dewey_file)
