70bbc6794130fa7fa32a774491cfab462912dc6a
Jia Le <jle26531@gmail.com>
2023-11-06 14:36:46 +0000

    Books Model

--- app/books.py
+++ app/books.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required
 from utils.currency import currency as cr
 from utils.dewey import dewey as dw
@@ -10,8 +10,11 @@ from .database import db
 books = Blueprint("books", __name__, template_folder="client", static_folder="static")
 
 @books.route("/", endpoint="home")
+@init_process
+@login_required
 def home():
-    return render_template("books/home.html", title="Books")
+    books = Books.query.all()
+    return render_template("books/home.html", title="Books", books=books, nav=True)
 
 @books.route("/new", endpoint="new_books", methods=["POST", "GET"])
 @init_process
@@ -48,5 +51,79 @@ def new_books():
         
     source = [{"name": "Donations"}, {"name": "Purchasing"}, {"name": "Other"}]
     
-    return render_template(
-        "books/new.html", title="Books", dewey=dw, currency_=cr, source=source, langs=lg)
+    return render_template("books/new.html", title="Books", dewey=dw, currency_=cr, source=source, langs=lg, nav=True)
+
+@books.route('/delete/<int:books_id>', endpoint='delete_books', methods=['POST'])
+@init_process
+@login_required
+def delete_books(books_id):
+    books_to_delete = Books.query.get(books_id)
+
+    if books_to_delete:
+        try:
+            db.session.delete(books_to_delete)
+            db.session.commit()
+            flash('Book Record deleted successfully!', category='success')
+        except:
+            flash('Failed to delete the Book Record !', category='error')
+    else:
+        flash('Book Record not found!', category='error')
+
+    return redirect(url_for('books.home'))
+
+@books.route('/update/<int:books_id>', endpoint='update_books', methods=['POST', 'GET'])
+@init_process
+@login_required
+def update_books(books_id):
+    
+    books_to_update = Books.query.get(books_id)
+
+    if not books_to_update:
+        flash("Book's Record not found!", category='error')
+        return redirect(url_for('books.home'))
+
+    if request.method == 'POST':
+        title = request.form.get("title")
+        author = request.form.get("author")
+        isbn = request.form.get("isbn")
+        dewey = request.form.get("dewey")
+        language = request.form.get("lang")
+        num_pages = request.form.get("page")
+        published_year = request.form.get("pub_year")
+        publisher = request.form.get("pub")
+        summary = request.form.get('summary')
+        barcode = request.form.get("barcode")
+        currency = request.form.get("currency")
+        price = request.form.get("price")
+        sources = request.form.get("sources")
+        sources_info = request.form.get("sources_info")
+
+        validate_barcode = Books.query.filter_by(barcode=barcode).first()
+
+        if validate_barcode and validate_barcode != books_to_update:
+            flash('Barcode Record cannot be duplicated, try again.', category='error')
+        else:
+            try:
+                books_to_update.title = title
+                books_to_update.author = author
+                books_to_update.isbn = isbn
+                books_to_update.dewey = dewey
+                books_to_update.language = language
+                books_to_update.num_pages = num_pages
+                books_to_update.published_year = published_year
+                books_to_update.publisher = publisher
+                books_to_update.summary = summary
+                books_to_update.barcode = barcode
+                books_to_update.currency = currency
+                books_to_update.price = price
+                books_to_update.sources = sources
+                books_to_update.sources_info = sources_info
+
+                db.session.commit()
+                flash('Books Record updated successfully!', category='success')
+            except:
+                flash('Failed to update the books record!', category='error')
+            return redirect(url_for('books.home'))
+        
+    source = [{"name": "Donations"}, {"name": "Purchasing"}, {"name": "Other"}]
+    return render_template('books/update.html', nav=True, title='Update Books Record', dewey=dw, currency_=cr, source=source, langs=lg, books=books_to_update)
