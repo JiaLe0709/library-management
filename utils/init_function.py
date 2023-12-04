1efabaa6cdc889875be036c139df90a64d20c050
Jia Le <jle26531@gmail.com>
2023-12-04 11:31:44 +0000

    fix: user db

--- utils/init_function.py
+++ utils/init_function.py
@@ -3,14 +3,14 @@ from app.models import User
 
 def init_process(func):
     def decorated_function(*args, **kwargs):
-        if User.query.first() is None:
+        if User.query.filter_by(id=1).first() is None:
             return redirect(url_for('init.get_started'))
         return func(*args, **kwargs)
     return decorated_function
 
 def remove_init(func):
     def decorated_function(*args, **kwargs):
-        if User.query.first() is not None:
+        if User.query.filter_by(id=1).first() is not None:
             abort(404)
         return func(*args, **kwargs)
     return decorated_function
